87e6ca503105d8897eae053318a2f0d9099b0bf4
Guenther Haeussermann <guenther@pmasolutions.com>
2018-03-06 21:02:33 -0500

    Add decimal package

--- lib/elixirpi.ex
+++ lib/elixirpi.ex
@@ -1,18 +1,20 @@
 defmodule Elixirpi do
-  @moduledoc """
-  Documentation for Elixirpi.
-  """
+  alias Decimal, as: D
 
-  @doc """
-  Hello world.
+  def term(n) do
+    D.div(D.new(1), D.new(n) |> D.mult(D.new(n+1)) |> D.mult(D.new(n+2)))
+  end
 
-  ## Examples
+  def run do
+    D.set_context(%D.Context{D.get_context | precision: 100}) 
 
-      iex> Elixirpi.hello
-      :world
+    pi = 1..10000
+      |> Enum.reduce(D.new(0), fn(i, acc) -> acc |> D.add(term(i*4-2)) |> D.sub(term(i*4)) end)
+      |> D.mult(D.new(4))
 
-  """
-  def hello do
-    :world
+
+    pi = D.add(pi, D.new(3))
+    IO.puts "PI is "
+    IO.inspect pi
   end
 end
