7ab5ae05346fdd6d2a685f75a9d4057857bfde79
Guenther Haeussermann <guenther@pmasolutions.com>
2018-03-06 20:58:05 -0500

    Initial commit

---
+++ lib/elixirpi.ex
@@ -0,0 +1,18 @@
+defmodule Elixirpi do
+  @moduledoc """
+  Documentation for Elixirpi.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elixirpi.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixirpi_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirpiTest do
+  use ExUnit.Case
+  doctest Elixirpi
+
+  test "greets the world" do
+    assert Elixirpi.hello() == :world
+  end
+end
