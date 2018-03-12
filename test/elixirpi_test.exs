80104fdb87151ac7106204281fb6b19b853ae251
Guenther Haeussermann <guenther@pmasolutions.com>
2018-03-11 22:32:23 -0400

    Remove 'hello' test

--- test/elixirpi_test.exs
+++ test/elixirpi_test.exs
@@ -2,7 +2,7 @@ defmodule ElixirpiTest do
   use ExUnit.Case
   doctest Elixirpi
 
-  test "greets the world" do
-    assert Elixirpi.hello() == :world
-  end
+  #test "greets the world" do
+  #  assert Elixirpi.hello() == :world
+  #end
 end
